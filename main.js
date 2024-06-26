c44d14b5e23349035c4b6a538356d1ee52ddb2f0
MisbahDayo <dayomuhammad892@gmail.com>
2024-06-26 23:42:43 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var teacher = {
+    name: "AYESHA",
+    exercise: "11"
+};
+console.log(teacher.name);
+console.log(teacher["exercise"]);
+// Type Declaration
+var student;
+student = {
+    name: "HAFSA",
+    age: 21
+};
+console.log(student["name"]);
+console.log(student.age);
+// type anonymous
+var car = {
+    color: "black",
+    model: 2024
+};
+console.log(car.color);
+console.log(car["model"]);
---
+++ main.ts
@@ -0,0 +1,24 @@
+
+// type anonymous
+let car :{color: string ,model : number}={
+    color : "black",
+    model: 2024
+}
+console.log(car.color);
+console.log(car["model"]);
+
+
+
+// Type Declaration
+let student : {
+    name: string,
+    age: number
+}
+student = {
+    name: "HAFSA",
+    age: 21
+}
+console.log(student["name"]);
+console.log(student.age);
+
+
